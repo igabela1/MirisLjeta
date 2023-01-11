de8cbe5ebe0eab0fd54eb2fa00cc8e50c06f47ba
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-01-12 00:26:49 +0100

    home fxml

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,14 +1,30 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/home.fxml"));
+        primaryStage.setTitle("Miris Ljeta!");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
 
-    {
-        System.out.println( "Hello World!" );
+        primaryStage.show();
     }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
