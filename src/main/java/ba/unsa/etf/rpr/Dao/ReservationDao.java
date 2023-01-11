cfab46e383c6da864ccab7fd4390947aff27daa1
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-01-11 11:16:00 +0100

    Reservation Dao

---
+++ src/main/java/ba/unsa/etf/rpr/Dao/ReservationDao.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.Dao;
+import ba.unsa.etf.rpr.Domain.Reservation;
+
+import java.util.List;
+
+public interface ReservationDao extends Dao<Reservation> {
+    // public List<Reservation> searchByNumberOfBeds();
+}
