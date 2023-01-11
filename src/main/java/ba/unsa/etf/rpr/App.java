1b828e34495c79d5f49708690116ef189786e748
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-01-11 10:58:40 +0100

    User domain

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -7,6 +7,7 @@ package ba.unsa.etf.rpr;
 public class App 
 {
     public static void main( String[] args )
+
     {
         System.out.println( "Hello World!" );
     }
---
+++ src/main/java/ba/unsa/etf/rpr/Domain/User.java
@@ -0,0 +1,84 @@
+package ba.unsa.etf.rpr.Domain;
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String username;
+    private String password;
+    private boolean userType;
+
+    public User(int id, String email, String username, String password, boolean userType) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isUserType() {
+        return userType;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserType(boolean userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && userType == user.userType && Objects.equals(email, user.email) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, username, password, userType);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", userType=" + userType +
+                '}';
+    }
+}
+
