7ab0e15bcf2d069a66fdde800da16707b6c78eca
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-01-11 11:08:42 +0100

    Room Bungalow domain

---
+++ src/main/java/ba/unsa/etf/rpr/Domain/Room_Bungalow.java
@@ -0,0 +1,67 @@
+package ba.unsa.etf.rpr.Domain;
+import java.util.Objects;
+
+public class Room_Bungalow {
+
+    private int id;
+
+    private boolean isAvailable;
+    private String roomDescription;
+
+
+    public Room_Bungalow(int id, boolean isAvailable, String roomDescription, int numberOfBeds) {
+        this.id = id;
+        this.isAvailable = isAvailable;
+        this.roomDescription = roomDescription;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public String getRoomDescription() {
+        return roomDescription;
+    }
+
+    public void setRoomDescription(String roomDescription) {
+        this.roomDescription = roomDescription;
+    }
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room_Bungalow that = (Room_Bungalow) o;
+        return id == that.id && isAvailable == that.isAvailable  && roomDescription.equals(that.roomDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isAvailable, roomDescription);
+    }
+
+    @Override
+    public String toString() {
+        return "Room_Bungalow{" +
+                "id=" + id +
+                ", isAvailable=" + isAvailable +
+                ", roomDescription='" + roomDescription + '\'' + '}';
+    }
+}
