164b6638be6b31616000042a82104127416b3483
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-01-15 23:35:58 +0100

    home controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class HomeController {
+    public Button RegistracijaDugmeId;
+    public Button PrijavaDugmeId;
+
+    public void Registracija(ActionEvent actionEvent) {
+    }
+
+    public void Prijava(ActionEvent actionEvent) {
+    }
+}
