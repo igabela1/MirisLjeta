627e048dd0c803fa2e6012c8777927e4262e8dfd
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-20 12:49:48 +0100

    RoomBungalowManager.java

---
+++ src/main/java/ba/unsa/etf/rpr/Business/RoomBungManager.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.Business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+        import ba.unsa.etf.rpr.Domain.Room_Bungalow;
+        import ba.unsa.etf.rpr.Exceptions.Room_BungalowException;
+
+        import java.util.List;
+
+public class RoomBungManager {
+    public void delete(int RoomId) throws Room_BungalowException {
+        try {
+            DaoFactory.room_bungalowDao().delete(RoomId);
+        } catch (Room_BungalowException e) {
+            if (e.getMessage().contains("FOREIGN KEY")) {
+                throw new Room_BungalowException("NO");
+            }
+            throw e;
+        }
+
+    }
+
+    public Room_Bungalow add(Room_Bungalow r) throws Room_BungalowException {
+        return DaoFactory.room_bungalowDao().add(r);
+    }
+
+    public Room_Bungalow getById(int id) throws Room_BungalowException {
+        return DaoFactory.room_bungalowDao().getById(id);
+    }
+
+    public Room_Bungalow update(Room_Bungalow r) throws Room_BungalowException {
+        return DaoFactory.room_bungalowDao().update(r);
+    }
+
+    public List<Room_Bungalow> getAll() throws Room_BungalowException {
+        return DaoFactory.room_bungalowDao().getAll();
+    }
+}
