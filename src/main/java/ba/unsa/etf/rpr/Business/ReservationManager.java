dbfe89e28b7f3bf437cbcd3c75ed533bf305b353
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-20 12:49:26 +0100

    ReservationManager.java

---
+++ src/main/java/ba/unsa/etf/rpr/Business/ReservationManager.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.Business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.Domain.Reservation;
+import ba.unsa.etf.rpr.Exceptions.Room_BungalowException;
+
+import java.util.List;
+
+public class ReservationManager {
+    public void delete(int ReservationId) throws Room_BungalowException {
+        try {
+            DaoFactory.reservationDao().delete(ReservationId);
+        } catch (Room_BungalowException e) {
+            if (e.getMessage().contains("FOREIGN KEY")) {
+                throw new Room_BungalowException("NO");
+            }
+            throw e;
+        }
+
+    }
+
+    public Reservation add(Reservation r) throws Room_BungalowException {
+        return DaoFactory.reservationDao().add(r);
+    }
+
+    public Reservation getById(int id) throws Room_BungalowException {
+        return DaoFactory.reservationDao().getById(id);
+    }
+
+    public Reservation update(Reservation r) throws Room_BungalowException {
+        return DaoFactory.reservationDao().update(r);
+    }
+
+    public List<Reservation> getAll() throws Room_BungalowException {
+        return DaoFactory.reservationDao().getAll();
+    }
+}
