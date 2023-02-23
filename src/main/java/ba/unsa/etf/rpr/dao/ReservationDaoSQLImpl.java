88eb211cd9cee36be3e2ed50e3fe150ec1790093
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-23 01:47:55 +0100

    Promjena

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDaoSQLImpl.java
@@ -1,6 +1,113 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.Domain.Reservation;
+import ba.unsa.etf.rpr.Domain.Room_Bungalow;
+import ba.unsa.etf.rpr.Domain.User;
+import ba.unsa.etf.rpr.Exceptions.Room_BungalowException;
+import ba.unsa.etf.rpr.Business.RoomBungManager;
+import ba.unsa.etf.rpr.Business.UserManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * MySQL Implementation of DAO
+
+ */
+public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements ReservationDao {
+
+    private static ReservationDaoSQLImpl instance = null;
+
+    private final RoomBungManager r = new RoomBungManager();
+
+    private final UserManager u = new UserManager();
+
+    private ReservationDaoSQLImpl() {
+        super("RESERVATIONS");
+    }
+
+    public static ReservationDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new ReservationDaoSQLImpl();
+        return instance;
+    }
+
+    /**
+     * Remove instance.
+     */
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
+    }
+
+    @Override
+    public Reservation row2object(ResultSet rs) throws Room_BungalowException{
+        try {
+            Reservation reservation = new Reservation();
+            reservation.setId(rs.getInt("id"));
+            reservation.setUsername(DaoFactory.userDao().getById(rs.getInt("user_id")));
+            reservation.setCheckIn(rs.getDate("checkIn").toLocalDate());
+            reservation.setCheckOut(rs.getDate("checkOut").toLocalDate());
+            return reservation;
+        } catch (Exception e) {
+            throw new Room_BungalowException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
+    @Override
+    public Map<String, Object> object2row(Reservation object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("checkIn",object.getCheckIn());
+        item.put("checkOut",object.getCheckOut());
+        item.put("user_id", object.getUsername().getId());
+        return item;
+    }
+
+
+    public List<Reservation> getAllForUser(User user){
+        List<Reservation> userReservations = new ArrayList<>();
+        // Connect to the database
+        try{
+            // Prepare a statement to execute the query
+            String query = "SELECT * FROM RESERVATIONS WHERE user_id = ?";
+            PreparedStatement statement = getConnection().prepareStatement(query);
+            statement.setObject(1, user.getId());
+            // Execute the query and get the result set
+            ResultSet resultSet = statement.executeQuery();
+            // Iterate over the result set and add each hotel to the list
+            while (resultSet.next()) {
+                LocalDate checkIn = resultSet.getDate("checkIn").toLocalDate();
+                LocalDate checkOut = resultSet.getDate("checkOut").toLocalDate();
+                int user_id = resultSet.getInt("user_id");
+                int room_id = resultSet.getInt("room_id");
+                Room_Bungalow room = r.getById(room_id);
+                User user2 = u.getById(user_id);
+                Reservation reservation = new Reservation(checkIn, checkOut, room, user2);
+                userReservations.add(reservation);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Room_BungalowException e) {
+            throw new RuntimeException(e);
+        }
+        // Return the list of hotels
+        return userReservations;
+    }
+}
+
+/*package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.Domain.Reservation;
 import ba.unsa.etf.rpr.Exceptions.Room_BungalowException;
 
 import java.sql.Connection;
@@ -53,7 +160,7 @@ public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements R
         return item;
     }
 }
-    /*//private int numberOfBeds;
+   /* private int numberOfBeds;
     private Connection connection;
     private String tableName;
 
@@ -75,7 +182,8 @@ public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements R
     public ReservationDaoSQLImpl() {
 
     }
-
+*/
+/*
     public static ReservationDao getInstance() {
     }
 
