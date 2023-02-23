9df2494b9db7e1dd0f8394aafdc9458ff0f84bbb
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-23 19:02:57 +0100

    promjena

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -4,6 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+/**
+ * Main class
+ */
+public class AppFX extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Home/SignIn.fxml"));
+        primaryStage.setTitle("Miris Ljeta");
+        primaryStage.setScene(new Scene(root,USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+/*package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -14,14 +41,14 @@ import java.util.Objects;
 /**
  * The type App fx.
  */
-public class AppFX extends Application
+/*public class AppFX extends Application
 {
     /**
      * The entry point of application.
      *
      * @param args the input arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // Here you can work with args - command line parameters
         Application.launch(args);
     }
@@ -36,4 +63,4 @@ public class AppFX extends Application
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setResizable(false);
         primaryStage.show();
-    }}
+    }}*/
