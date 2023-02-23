342156a57a99163098f90bda93602cf63534d7a8
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-23 11:16:58 +0100

    Promjena

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.Domain.Reservation;
+import ba.unsa.etf.rpr.Domain.User;
 
+import java.sql.SQLException;
+import java.util.List;
 
 import ba.unsa.etf.rpr.Domain.Reservation;
 
 public interface ReservationDao extends Dao<Reservation> {
-
-
+    public List<Reservation> getAllForUser(User user) throws SQLException;
 }
 
