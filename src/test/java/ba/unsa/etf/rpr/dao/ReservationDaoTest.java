1f515643cfc835acf7b47bb04bc1f18a1de259d9
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-23 19:25:10 +0100

    ReservationDaoTest.java

---
+++ src/test/java/ba/unsa/etf/rpr/dao/ReservationDaoTest.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.Domain.Reservation;
+import ba.unsa.etf.rpr.Domain.Room_Bungalow;
+import ba.unsa.etf.rpr.Domain.User;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ReservationDaoTest {
+    @Test
+    void gettersAndSettersTest() {
+        LocalDate checkIn = LocalDate.of(2022, Month.FEBRUARY, 15);
+        LocalDate checkOut = LocalDate.of(2022, Month.FEBRUARY, 23);
+        Reservation reservation = new Reservation();
+        reservation.setId(1);
+        reservation.setCheckIn(checkIn);
+        reservation.setCheckOut(checkOut);
+        reservation.setRoomId(new Room_Bungalow());
+        reservation.setUsername(new User());
+
+        assertEquals(1, reservation.getId());
+        assertEquals(LocalDate.of(2022, Month.FEBRUARY, 15), reservation.getCheckIn());
+        assertEquals(LocalDate.of(2022, Month.FEBRUARY, 23), reservation.getCheckOut());
+        assertNotNull(reservation.getRoomId());
+        assertNotNull(reservation.getUsername());
+    }
+
+}
+
