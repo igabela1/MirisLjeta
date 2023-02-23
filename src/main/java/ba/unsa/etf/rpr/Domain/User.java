fbd84df261bf6ab44d140953362924b7a14ea75c
Ilhana Gabela <igabela1@etf.unsa.ba>
2023-02-23 01:51:29 +0100

    Promjena

--- src/main/java/ba/unsa/etf/rpr/Domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/Domain/User.java
@@ -1,117 +1,127 @@
 package ba.unsa.etf.rpr.Domain;
-import ba.unsa.etf.rpr.Domain.Idable;
 
-import java.util.Objects;
-
-public class User implements Idable {
-
+/**
+ * bean for user
+ */
+public class User implements Idable{
     private int id;
+    private String firstName;
+    private String lastName;
     private String email;
     private String username;
     private String password;
-    private String lastName;
     private int isAdministrator;
-    private String firstName;
 
-    public User(int id, String email, String username, String password, int isAdministrator) {
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName, String username, String password, int isAdministrator) {
         this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.username = username;
         this.password = password;
         this.isAdministrator = isAdministrator;
-    }
-
-    public User() {
-
     }
 
     public int getId() {
         return id;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && isAdministrator == user.isAdministrator && Objects.equals(email, user.email) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, username, password, isAdministrator);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", userType=" + isAdministrator +
-                '}';
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
-
-    public void setisAdministrator(int isAdministrator) {
-        this.isAdministrator = isAdministrator;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-
-
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int isAdministrator() {
+        return isAdministrator;
+    }
+
+    public void setisAdministrator(int administrator) {
+        isAdministrator = administrator;
+    }
     public int getisAdministrator() {
         return isAdministrator;
     }
-}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (isAdministrator != user.isAdministrator) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        if (!lastName.equals(user.lastName)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!username.equals(user.username)) return false;
+        return password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + isAdministrator;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", isAdministrator=" + isAdministrator +
+                '}';
+    }
+}
