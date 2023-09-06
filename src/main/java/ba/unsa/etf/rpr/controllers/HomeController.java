f92158a30f03420b2d0caeb10056ec0e4486ab42
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 17:18:43 +0200

    added logic to functions

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,149 +1,33 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
+
 public class HomeController {
 
-
-
-    @FXML
-    public Button aboutUsButton;
-
-    @FXML
-    public Button myProfileButton;
-    @FXML
-    public Button signUpButton;
-    @FXML
-    public Button signInButton;
-    @FXML
-    private Button bookButton = new Button();
-    @FXML
-    public Button reservationsButton;
+    @FXML private Button aboutUsButton, signInButton, signUpButton, closeButton;
     private User user = new User();
-    @FXML
-    public void setUser(User user) {
+    private final Utils utils = new Utils();
+
+    public HomeController() {}
+
+    public HomeController(User user) {
         this.user = user;
     }
-    @FXML
-    public User getUser() {
-        return user;
-    }
-   public HomeController() {
-    }
-    public HomeController(User finalUser){
-        this.user = finalUser;
+
+    public void signInOnAction(ActionEvent actionEvent) throws IOException {
+        utils.changeWindow(signInButton, "Sign In", "/fxml/SignIn.fxml", new SignInController());
     }
 
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+        utils.changeWindow(signUpButton, "Sign Up", "/fxml/SignUp.fxml", new SignUpController());
+    }
 
-
-    @FXML
-    public void initialize() {
-        aboutUsButton.setOnAction(event -> {
-            // Exit current window
-            Stage stage = (Stage) aboutUsButton.getScene().getWindow();
-            stage.close();
-            // Open the about camp page window
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fxml/Home/AboutCamp.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage aboutUsStage = new Stage();
-                aboutUsStage.initStyle(StageStyle.TRANSPARENT);
-                Scene scene = new Scene(root);
-                scene.setFill(Color.TRANSPARENT);
-                aboutUsStage.setScene(scene);
-                aboutUsStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-     myProfileButton.setOnAction(event -> {
-            // Exit current window
-            Stage stage = (Stage) myProfileButton.getScene().getWindow();
-            stage.close();
-            // Open the profile page window
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fxml/Home/MyProfile.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage aboutUsStage = new Stage();
-                aboutUsStage.initStyle(StageStyle.TRANSPARENT);
-                Scene scene = new Scene(root);
-                scene.setFill(Color.TRANSPARENT);
-                aboutUsStage.setScene(scene);
-                aboutUsStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        signInButton.setOnAction(event -> {
-            // Exit current window
-            Stage stage = (Stage) signInButton.getScene().getWindow();
-            stage.close();
-            // Open the profile page window
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fxml/Home/SignIn.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage aboutUsStage = new Stage();
-                aboutUsStage.initStyle(StageStyle.TRANSPARENT);
-                Scene scene = new Scene(root);
-                scene.setFill(Color.TRANSPARENT);
-                aboutUsStage.setScene(scene);
-                aboutUsStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        signUpButton.setOnAction(event -> {
-            // Exit current window
-            Stage stage = (Stage) signUpButton.getScene().getWindow();
-            stage.close();
-            // Open the profile page window
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fxml/Home/SignUp.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage aboutUsStage = new Stage();
-                aboutUsStage.initStyle(StageStyle.TRANSPARENT);
-                Scene scene = new Scene(root);
-                scene.setFill(Color.TRANSPARENT);
-                aboutUsStage.setScene(scene);
-                aboutUsStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-       /*// reservationsButton.setOnAction(event -> {
-            // Close the current window
-         //   Stage stage = (Stage) reservationsButton.getScene().getWindow();
-           // stage.close();
-            // Open the about us page window
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Home/MyProfile.fxml"));
-               // ReservationListController controller = new ReservationListController(user);
-                //fxmlLoader.setController(controller);
-                Parent root = fxmlLoader.load();
-                Stage stage2 = new Stage();
-               // stage2.getIcons().add(new Image("images/"));
-                stage2.initStyle(StageStyle.TRANSPARENT);
-                Scene scene = new Scene(root);
-                scene.setFill(Color.TRANSPARENT);
-                stage2.setScene(scene);
-                stage2.show();
-              //  stage.hide();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-        }
+    public void aboutUsOnAction(ActionEvent actionEvent) throws IOException {
+        utils.changeWindow(aboutUsButton, "About Us", "/fxml/AboutCamp.fxml", new AboutCampController());
+    }
 }
-
-
-
