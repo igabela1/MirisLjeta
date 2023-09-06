75ac29616bc771ce1a05fb95eea4ecd1fb6fe3d5
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 14:19:52 +0200

    changed the implementation of functions

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -8,8 +8,8 @@ import java.util.TreeMap;
 
 public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
     private static UserDaoSQLImpl instance = null;
-    public UserDaoSQLImpl() {
-        super("USERS");
+    private UserDaoSQLImpl() {
+        super("users");
     }
 
 
@@ -25,15 +25,15 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
     }
 
     @Override
-    public User row2object(ResultSet rs) throws Room_BungalowException {
+    public User row2object(ResultSet rs) throws Room_BungalowException{
         try {
             User person = new User();
             person.setId(rs.getInt("id"));
             person.setUsername(rs.getString("username"));
-            person.setFirstName(rs.getString("first_name"));
-            person.setLastName(rs.getString("last_name"));
+            person.setFirstName(rs.getString("firstName"));
+            person.setLastName(rs.getString("lastName"));
             person.setEmail(rs.getString("email"));
-            person.setisAdministrator(rs.getInt("role"));
+            person.setRole(rs.getInt("isAdministrator"));
             person.setPassword(rs.getString("password"));
             return person;
         } catch (Exception e) {
@@ -41,27 +41,30 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         }
     }
 
-
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
     @Override
     public Map<String, Object> object2row(User object) {
         Map<String, Object> item = new TreeMap<>();
         item.put("id", object.getId());
         item.put("username", object.getUsername());
-        item.put("first_name", object.getFirstName());
-        item.put("last_name", object.getLastName());
+        item.put("firstName", object.getFirstName());
+        item.put("lastName", object.getLastName());
         item.put("email", object.getEmail());
-        item.put("isAdministrator", object.getisAdministrator());
+        item.put("isAdministrator", object.getRole());
         item.put("password", object.getPassword());
         return item;
     }
 
     @Override
     public User findUsername(String usernameField) throws Room_BungalowException{
-        String insert = "SELECT id from USERS where username='" + usernameField +"'";
+        String find = "SELECT id from users where username='" + usernameField +"'";
         try {
-            PreparedStatement stmt = getConnection().prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = getConnection().prepareStatement(find, Statement.RETURN_GENERATED_KEYS);
             ResultSet rs = stmt.executeQuery();
-            if(rs.next()) {
+            if(rs.next()) { // result set is iterator.
                 return getById(rs.getInt(1));
             }
             rs.close();
@@ -70,59 +73,14 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         }
         return null;
     }
-}
-    /*private Connection connection;
-    private String tableName;
 
-    public UserDaoSQLImpl(String tableName) {
-        try{
-            this.tableName = tableName;
-            Properties p = new Properties();
-            p.load(ClassLoader.getSystemResource("application.properties").openStream());
-            String url = p.getProperty("db.connection_string");
-            String username = p.getProperty("db.username");
-            String password = p.getProperty("db.password");
-            this.connection = DriverManager.getConnection(url, username, password);
-        }catch (Exception e){
-            e.printStackTrace();
-            System.exit(0);
+    public int totalUsers() throws SQLException{
+        int total = 0;
+        String query = "SELECT count(username) AS total_users FROM users";
+        try (PreparedStatement st = AbstractDao.getConnection().prepareStatement(query)) {
+            ResultSet result = st.executeQuery();
+            if (result.next()) total = result.getInt("total_users");
         }
+        return total;
     }
-
-    public static UserDao getInstance() {
-    }
-
-    public Connection getConnection(){
-        return this.connection;
-    }
-
-    @Override
-    public User add(User item) {
-        return null;
-    }
-
-    @Override
-    public User update(User item) {
-        return null;
-    }
-
-    @Override
-    public void delete(int id) {
-
-    }
-
-    @Override
-    public List<User> getAll() {
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDaoSQLImpl{}";
-    }
-
-    public UserDaoSQLImpl() {
-    }
-
 }
-*/
