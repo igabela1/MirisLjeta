ee626a3ec7b3ef51ca2a5262baf2354143ac9840
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 17:20:15 +0200

    changed the start of the app

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,14 +1,18 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.controllers.HomeController;
+import ba.unsa.etf.rpr.controllers.Utils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.util.Objects;
 
 /**
  * The type App fx.
@@ -21,18 +25,43 @@ public class AppFX extends Application
      * @param args the input arguments
      */
     public static void main(String[] args) {
-
         Application.launch(args);
     }
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/fxml/Home/home.fxml")));
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Home.fxml"));
+        HomeController controller = new HomeController();
+        fxmlLoader.setController(controller);
+        Parent root = fxmlLoader.load();
 
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.setResizable(false);
+
+        // window icon
+        primaryStage.getIcons().add(new Image("images/logoTransparent.png"));
+
+        // mouse press and drag listeners for window dragging
+        root.setOnMousePressed(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
         primaryStage.show();
     }}
