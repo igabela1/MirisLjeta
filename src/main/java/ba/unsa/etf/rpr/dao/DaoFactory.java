870db8fd4984c5044c40141c230dbe5a4e20f277
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 14:16:55 +0200

    changed the declaration of functions

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -2,28 +2,22 @@ package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
 
-    private static final Room_BungalowDao Room_BungalowDao = Room_BungalowDaoSQLImpl.getInstance();
-
-    private static final UserDao UserDao = UserDaoSQLImpl.getInstance();
-    private static final ReservationDao ReservationDao = ReservationDaoSQLImpl.getInstance();
+    private static final Room_BungalowDao room_BungalowDao = Room_BungalowDaoSQLImpl.getInstance();
+    private static final UserDao userDao = UserDaoSQLImpl.getInstance();
+    private static final ReservationDao reservationDao = ReservationDaoSQLImpl.getInstance();
 
 
     private DaoFactory(){
     }
 
     public static Room_BungalowDao room_bungalowDao(){
-        return Room_BungalowDao;
+        return room_BungalowDao;
     }
-
     public static ReservationDao reservationDao(){
-        return ReservationDao;
+        return reservationDao;
     }
-
-
     public static UserDao userDao(){
-        return UserDao;
+        return userDao;
     }
-
-
 
 }
--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface ReservationDao extends Dao<Reservation> {
+    public int totalIncome() throws SQLException;
     public List<Reservation> getAllForUser(User user) throws SQLException;
 }
 
