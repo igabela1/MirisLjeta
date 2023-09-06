554cf479c7d90019e0c77b69998b83e3d3b0713b
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 14:20:06 +0200

    changed the declaration of functions

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -2,6 +2,9 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.Room_BungalowException;
 
+import java.sql.SQLException;
+
 public interface UserDao extends Dao<User> {
     User findUsername(String usernameField) throws Room_BungalowException;
+    public int totalUsers() throws SQLException;
 }
