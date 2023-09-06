32abb537905dc279eda581148bcf85c1725036cc
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 17:19:38 +0200

    added a room manager

--- src/main/java/ba/unsa/etf/rpr/business/RoomBungManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/RoomBungManager.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.domain.Room_Bungalow;
-import ba.unsa.etf.rpr.exceptions.Room_BungalowException;
+        import ba.unsa.etf.rpr.domain.Room_Bungalow;
+        import ba.unsa.etf.rpr.exceptions.Room_BungalowException;
 
 import java.sql.SQLException;
 import java.util.List;
