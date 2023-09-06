2d630b987793c5e0f556c3eafdeb6216d327724d
igabela1 <igabela1@etf.unsa.ba>
2023-09-06 17:18:15 +0200

    added logic to functions

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutCampController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutCampController.java
@@ -1,48 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.domain.User;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class AboutCampController {
 
-    @FXML
-    private ImageView goBack;
+    @FXML private ImageView goBack;
+    private final Utils utils = new Utils();
 
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
+    public void initialize(){
+        goBack.setOnMouseClicked(event -> utils.changeWindow(goBack, "Home Page", "/fxml/Home.fxml", new HomeController()));
     }
 
-    private User user;
-
-    public void initialize() {
-        goBack.setOnMouseClicked(event -> {
-            // Close current window
-            Stage stage = (Stage) goBack.getScene().getWindow();
-            stage.close();
-            // Open previous window
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fxml/Home/home.fxml"));
-                HomeController controller = new HomeController();
-                controller.setUser(user);
-                fxmlLoader.setController(controller);
-                Parent root = fxmlLoader.load();
-                Stage homeStage = new Stage();
-                homeStage.setScene(new Scene(root));
-                homeStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-    }
 }
