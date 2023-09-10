4da06e4a0c5186a73de62b011c55342f80fd90ba
igabela1 <igabela1@etf.unsa.ba>
2023-09-10 15:40:18 +0200

    implemented functions for admin account

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminAccountController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminAccountController.java
@@ -11,94 +11,152 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class AdminAccountController {
 
-    @FXML private Button myProfileButton;
+    @FXML private Button myProfileButton, addRoomButton, updateRoomButton, deleteRoomButton, addReservationButton, updateReservationButton, deleteReservationButton;
     @FXML private ImageView logOutButton;
-    @FXML private Label nameLabel;
+    @FXML private Label nameLabel, totalIncome, totalRooms;
     public TableView<User> usersTable;
     public TableView<Room_Bungalow> roomsTable;
     public TableView<Reservation> reservationsTable;
     public TableColumn<User, Integer> idUserColumn;
-    public TableColumn<User, String> firstNameColumn;
-    public TableColumn<User, String> lastNameColumn;
-    public TableColumn<User, String> emailColumn;
-    public TableColumn<User, String> usernameColumn;
-    public TableColumn<Room_Bungalow, Integer> idRoomColumn;
+    public TableColumn<User, String> firstNameColumn, lastNameColumn, emailColumn, usernameColumn;
+    public TableColumn<Room_Bungalow, Integer> idRoomColumn, capacityColumn;
     public TableColumn<Room_Bungalow, Double> pricePerNightColumn;
-    public TableColumn<Room_Bungalow, Integer> capacityColumn;
     public TableColumn<Room_Bungalow, Boolean> statusColumn;
-    public TableColumn<Reservation, Integer> reservationIdColumn;
-    public TableColumn<Reservation, LocalDate> checkInColumn;
-    public TableColumn<Reservation, LocalDate> checkOutColumn;
-    public TableColumn<Reservation, Integer> roomIdColumn;
+    public TableColumn<Reservation, Integer> reservationIdColumn, roomIdColumn;
+    public TableColumn<Reservation, LocalDate> checkInColumn, checkOutColumn;
     public TableColumn<Reservation, Double> totalColumn;
     public TableColumn<Reservation, String> userColumn;
     private final User user;
+    private final ReservationManager r = new ReservationManager();
+    private final RoomBungManager rm = new RoomBungManager();
     public AdminAccountController(User u){
         this.user = u;
     }
     private final Utils utils = new Utils();
-    public void initialize() {
+    public void initialize() throws SQLException {
 
         myProfileButton.setOnMouseClicked(event -> utils.changeWindow(myProfileButton, "My Profile", "/fxml/MyProfile.fxml", new MyProfileController(user)));
         logOutButton.setOnMouseClicked(event -> utils.changeWindow(logOutButton, "Main Page", "/fxml/Home.fxml", new HomeController()));
         nameLabel.setText(user.getFirstName());
 
-        idUserColumn.setCellValueFactory(new PropertyValueFactory<User, Integer>("id"));
-        firstNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("firstName"));
-        lastNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("lastName"));
-        emailColumn.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
-        usernameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
-        idRoomColumn.setCellValueFactory(new PropertyValueFactory<Room_Bungalow, Integer>("id"));
-        pricePerNightColumn.setCellValueFactory(new PropertyValueFactory<Room_Bungalow, Double>("pricePerNight"));
-        capacityColumn.setCellValueFactory(new PropertyValueFactory<Room_Bungalow, Integer>("capacity"));
-        statusColumn.setCellValueFactory(new PropertyValueFactory<Room_Bungalow, Boolean>("status"));
-        reservationIdColumn.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("id"));
-        checkInColumn.setCellValueFactory(new PropertyValueFactory<Reservation, LocalDate>("checkIn"));
-        checkOutColumn.setCellValueFactory(new PropertyValueFactory<Reservation, LocalDate>("checkOut"));
-        roomIdColumn.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("roomID"));
-        totalColumn.setCellValueFactory(new PropertyValueFactory<Reservation, Double>("total"));
-        userColumn.setCellValueFactory(new PropertyValueFactory<Reservation, String>("userID"));
+        idUserColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        idRoomColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        pricePerNightColumn.setCellValueFactory(new PropertyValueFactory<>("pricePerNight"));
+        capacityColumn.setCellValueFactory(new PropertyValueFactory<>("capacity"));
+        // statusColumn.setCellValueFactory(new PropertyValueFactory<Room_Bungalow, Boolean>("status"));
+        reservationIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        checkInColumn.setCellValueFactory(new PropertyValueFactory<>("checkIn"));
+        checkOutColumn.setCellValueFactory(new PropertyValueFactory<>("checkOut"));
+        roomIdColumn.setCellValueFactory(new PropertyValueFactory<>("roomId"));
+        totalColumn.setCellValueFactory(new PropertyValueFactory<>("total"));
+        userColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+        statusColumn.setCellFactory(column -> new TableCell<Room_Bungalow, Boolean>() {
+            private final ImageView imageView = new ImageView();
+
+            @Override
+            protected void updateItem(Boolean item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null || empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    Boolean statusValue = getTableView().getItems().get(getIndex()).getStatus();
+                    imageView.setFitWidth(20);
+                    imageView.setFitHeight(20);
+                    imageView.setImage(statusValue ? new Image("/images/trueIcon.png") : new Image("/images/falseIcon.png"));
+                    setGraphic(imageView);
+                }
+            }
+        });
 
         refreshTables();
+
+        totalIncome.setText(r.totalIncome() +" $");
+        totalRooms.setText(String.valueOf(rm.totalRooms()));
+
+        updateRoomButton.setOnMouseClicked(event -> {
+            Room_Bungalow selectedRoom = roomsTable.getSelectionModel().getSelectedItem();
+            if (selectedRoom == null) {
+                // Prikazati upozorenje korisniku da nijedna soba nije izabrana
+                showAlert("Please select a room to update.");
+            } else {
+                utils.openDialog("Update Room", "/fxml/Admin/Room/updateRoomDialog.fxml", new UpdateRoomController(this, user, selectedRoom));
+            }
+        });
+
+        deleteRoomButton.setOnMouseClicked(event -> {
+            Room_Bungalow selectedRoom = roomsTable.getSelectionModel().getSelectedItem();
+                if (selectedRoom == null) {
+                    // Prikazati upozorenje korisniku da nijedna soba nije izabrana
+                    showAlert("Please select a room to update.");
+                } else {
+                    utils.openDialog("Delete Room", "/fxml/Admin/Room/deleteRoomDialog.fxml", new DeleteRoomController(this, user, selectedRoom));
+                }
+        });
+
+        updateReservationButton.setOnMouseClicked(event -> {
+            Reservation selectedReservation = reservationsTable.getSelectionModel().getSelectedItem();
+
+            if (selectedReservation == null) {
+                // Prikazati upozorenje korisniku da nijedna rezervacija nije izabrana
+                showAlert("Please select a reservation to update.");
+            } else {
+                // Assuming you have a method to open a dialog for reservation update
+                utils.openDialog("Update Reservation", "/fxml/Admin/Reservation/updateReservationDialog.fxml", new UpdateReservationController(this, user, selectedReservation));
+            }
+        });
+
+        deleteReservationButton.setOnMouseClicked(event -> {
+            Reservation selectedReservation = reservationsTable.getSelectionModel().getSelectedItem();
+
+            if (selectedReservation == null) {
+                // Prikazati upozorenje korisniku da nijedna rezervacija nije izabrana
+                showAlert("Please select a reservation to delete.");
+            } else {
+                // Assuming you have a method to open a dialog for reservation deletion
+                utils.openDialog("Delete Reservation", "/fxml/Admin/Reservation/deleteReservationDialog.fxml", new DeleteReservationController(this, user, selectedReservation));
+            }
+        });
+
+
+        addRoomButton.setOnMouseClicked(event -> utils.openDialog("Add Room", "/fxml/Admin/Room/addRoomDialog.fxml", new AddRoomController(this, user)));
+        addReservationButton.setOnMouseClicked(event -> utils.openDialog("Add Reservation", "/fxml/Admin/Reservation/addReservationDialog.fxml", new AddReservationController(this, user)));
+
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     /**
      * fetch data from DB
      */
-    void refreshTables() {
+    public void refreshTables() {
         try {
             // Fetch data from your data source (e.g., UserManager, RoomManager and ReservationManager)
             List<User> userList = UserManager.getAll();
             List<Room_Bungalow> roomList = RoomBungManager.getAll();
             List<Reservation> reservationList = ReservationManager.getAll();
-
-
-            // Ispis elemenata iz roomList
-            System.out.println("Rooms:");
-            for (Room_Bungalow room : roomList) {
-                System.out.println("ID: " + room.getId() +
-                        ", Price Per Night: " + room.getPricePerNight() +
-                        ", Status: " + room.getStatus() +
-                        ", Capacity: " + room.getCapacity());
-            }
-
-            // Ispis elemenata iz reservationList
-            System.out.println("Reservations:");
-            for (Reservation reservation : reservationList) {
-                System.out.println("ID: " + reservation.getId() +
-                        ", Check-In: " + reservation.getCheckIn() +
-                        ", Check-Out: " + reservation.getCheckOut() +
-                        ", Room ID: " + reservation.getRoomId() +
-                        ", Total: " + reservation.getTotal() +
-                        ", Username: " + reservation.getUsername());
-            }
 
             // Update the table views with the new data
             usersTable.setItems(FXCollections.observableList(userList));
